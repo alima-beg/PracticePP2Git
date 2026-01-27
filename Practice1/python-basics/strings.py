b3cedead4e85ce768fbf7b4c513a60a66e0c64cb
Alima Begimbek <alima.beg@mail.ru>
2026-01-27 22:16:37 +0300

    Коммит для strings.py

--- Practice1/python-basics/strings.py
+++ Practice1/python-basics/strings.py
@@ -3,7 +3,6 @@ print('Hello')
 
 print("It's alright")
 print("He is called 'Johnny'")
-print('He is called "Johnny"')
 
 a = "Hello"
 print(a)
@@ -12,12 +11,6 @@ a = """Lorem ipsum dolor sit amet,
 consectetur adipiscing elit,
 sed do eiusmod tempor incididunt
 ut labore et dolore magna aliqua."""
-print(a)
-
-a = '''Lorem ipsum dolor sit amet,
-consectetur adipiscing elit,
-sed do eiusmod tempor incididunt
-ut labore et dolore magna aliqua.'''
 print(a)
 
 a = "Hello, World!"
